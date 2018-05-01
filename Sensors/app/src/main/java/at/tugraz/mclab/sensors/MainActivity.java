3027654c5e28d16758f6a9b1769a094d3afd7a70
Christian <christian.toth@student.tugraz.at>
2018-05-01 23:25:54 +0200

    listing of all available sensors in the app (following https://google-developer-training.gitbooks.io/android-developer-advanced-course-practicals/unit-1-expand-the-user-experience/lesson-3-sensors/3-1-p-working-with-sensor-data/3-1-p-working-with-sensor-data.html)

--- Sensors/app/src/main/java/at/tugraz/mclab/sensors/MainActivity.java
+++ Sensors/app/src/main/java/at/tugraz/mclab/sensors/MainActivity.java
@@ -1,12 +1,22 @@
 package at.tugraz.mclab.sensors;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        StringBuilder sensorText = new StringBuilder();
+
+        for (Sensor sensor : deviceSensors ) {
+            sensorText.append(sensor.getName()).append(
+                    System.getProperty("line.separator"));
+        }
+
+        TextView sensorTextView = (TextView) findViewById(R.id.sensor_list);
+        sensorTextView.setText(sensorText);
 
     }
 
