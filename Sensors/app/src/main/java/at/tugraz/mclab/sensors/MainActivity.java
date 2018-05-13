aaa68628adafc3a24607f7b9cc660ee43e7417db
Christian <christian.toth@student.tugraz.at>
2018-05-13 15:41:37 +0200

    whatever

--- Sensors/app/src/main/java/at/tugraz/mclab/sensors/MainActivity.java
+++ Sensors/app/src/main/java/at/tugraz/mclab/sensors/MainActivity.java
@@ -3,47 +3,76 @@ package at.tugraz.mclab.sensors;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
-import android.hardware.SensorManager;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.util.List;
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Map;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    private static int BUF_SIZE = 48;
+    private static int START_DELAY = 1500; // start delay in ms
+    private static int PERIOD = 1000; // scheduling period in ms
+
     private SensorManager mSensorManager;
-    private StringBuilder sensorList;
-    private String accelerometerText;
-    private String gyroscopeText;
-    private String gravitySensorText;
-    private String linearAccelerationText;
     private TextView sensorTextView;
-    private Sensor accelerometer;
-    private Sensor gyroscope;
-    private Sensor gravitySensor;
     private Sensor linearAccelerationSensor;
-    private boolean readSensors = false;
+    private ArrayDeque<Double> xbuffer;
+    private ArrayDeque<Double> ybuffer;
+    private ArrayDeque<Double> zbuffer;
+    private final Timer timerUIUpdate = new Timer("timerUIUpdate");
+    private final TimerTask taskUIUpdate = new UIUpdateThread();
+    private final ReentrantLock bufferLock = new ReentrantLock();
+    private final FeatureExtractor featureExtractor = new FeatureExtractor();
+
     File dataFile;
+
+    private class UIUpdateThread extends TimerTask {
+        @Override
+        public void run() {
+
+            double[] x = new double[BUF_SIZE], y = new double[BUF_SIZE], z = new double[BUF_SIZE];
+            bufferLock.lock();
+            try {
+                int i = 0;
+                for (Iterator<Double> it = xbuffer.iterator(); it.hasNext(); ) {
+                    x[i++] = it.next();
+                }
+                i = 0;
+                for (Iterator<Double> it = ybuffer.iterator(); it.hasNext(); ) {
+                    y[i++] = it.next();
+                }
+                i = 0;
+                for (Iterator<Double> it = zbuffer.iterator(); it.hasNext(); ) {
+                    z[i++] = it.next();
+                }
+            } finally {
+                bufferLock.unlock();
+            }
+
+            final double[] features = featureExtractor.extractFeatureVectore(x, y, z);
+            // Todo : classification
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String bla = "asdf" + features[0] + "bla" + features[3];
+                    sensorTextView.setText(bla);
+                }
+            });
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,32 +83,26 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         // init sensor manager and get (list of all) sensors
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        gyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        gravitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         linearAccelerationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-
-        // build sensor list
-        sensorList = new StringBuilder();
-        for (Sensor sensor : deviceSensors) {
-            sensorList.append(sensor.getName()).append(System.getProperty("line.separator"));
-        }
 
         // init text view
         sensorTextView = (TextView) findViewById(R.id.sensor_text);
-        sensorTextView.setText("Choose an option from the menu..");
+        sensorTextView.setText("Waiting for activity..");
+
+        xbuffer = new ArrayDeque(BUF_SIZE);
+        ybuffer = new ArrayDeque(BUF_SIZE);
+        zbuffer = new ArrayDeque(BUF_SIZE);
 
         // init file for sensor data
-        dataFile = new File(getExternalFilesDir(null), "sensorData.txt");
-        try {
-            dataFile.delete();
-            dataFile.createNewFile();
-            dataFile.setWritable(true, false);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        //        dataFile = new File(getExternalFilesDir(null), "sensorData.txt");
+        //        try {
+        //            dataFile.delete();
+        //            dataFile.createNewFile();
+        //            dataFile.setWritable(true, false);
+        //        } catch (IOException e) {
+        //            e.printStackTrace();
+        //        }
+        //
     }
 
     @Override
@@ -89,18 +112,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // SENSOR_DELAY_NORMAL .... fs =  5,00 Hz
         // SENSOR_DELAY_UI ........ fs = 16,67 Hz
         // SENSOR_DELAY_GAME ...... fs = 50,00 Hz
-        // if (accelerometer != null) {
-        //     mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
-        // }
-        // if (gyroscope != null) {
-        //     mSensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
-        // }
-        // if (gravitySensor != null) {
-        //     mSensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_NORMAL);
-        // }
         if (linearAccelerationSensor != null) {
-            mSensorManager.registerListener(this, linearAccelerationSensor, SensorManager.SENSOR_DELAY_GAME);
+            mSensorManager.registerListener(this, linearAccelerationSensor, SensorManager
+                    .SENSOR_DELAY_GAME);
         }
+
+        timerUIUpdate.scheduleAtFixedRate(taskUIUpdate, START_DELAY, PERIOD);
     }
 
     @Override
@@ -111,60 +128,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        double x = 0, y = 0, z = 0;
 
-        if (readSensors == false)
-            return;
-
+        // write the measurement data into buffers
         int sensorType = event.sensor.getType();
-        switch (sensorType) {
-            case Sensor.TYPE_ACCELEROMETER:
-                x = event.values[0];
-                y = event.values[1];
-                z = event.values[2];
-                accelerometerText = "Accelerometer:\nx = " + x + "\ny = " + y + "\nz " + "= " + "" + z + "\n\n";
-                break;
+        if (sensorType == Sensor.TYPE_LINEAR_ACCELERATION) {
 
-            case Sensor.TYPE_GYROSCOPE:
-                x = event.values[0];
-                y = event.values[1];
-                z = event.values[2];
-                gyroscopeText = "Gyroscope:\nx = " + x + "\ny = " + y + "\nz = " + z + "\n\n";
-                break;
+            bufferLock.lock();
+            try {
+                if (!xbuffer.isEmpty())
+                    xbuffer.removeLast();
+                xbuffer.addFirst((double) event.values[0]);
+                if (!ybuffer.isEmpty())
+                    ybuffer.removeLast();
+                ybuffer.addFirst((double) event.values[1]);
+                if (!zbuffer.isEmpty())
+                    zbuffer.removeLast();
+                zbuffer.addFirst((double) event.values[2]);
 
-            case Sensor.TYPE_GRAVITY:
-                x = event.values[0];
-                y = event.values[1];
-                z = event.values[2];
-                gravitySensorText = "Gravity Sensor:\nx = " + x + "\ny = " + y + "\nz = " + z + "\n\n";
-                break;
+            } finally {
+                bufferLock.unlock();
+            }
 
-            case Sensor.TYPE_LINEAR_ACCELERATION:
-                x = event.values[0];
-                y = event.values[1];
-                z = event.values[2];
-                linearAccelerationText = "Linear Acceleration:\nx = " + x + "\ny = " + y + "\nz = " + z + "\n\n";
-                break;
-
-            default:
-        }
-
-        sensorTextView.setText(linearAccelerationText);
-
-        // write sensor data to log file
-        try (OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(dataFile, true), "UTF-8")) {
-            osw.write(x + ";" + y + ";" + z + "\n");
-            osw.flush();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 
@@ -181,26 +171,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         int id = item.getItemId();
         switch (id) {
             case R.id.action_list_sensors:
-                readSensors = false;
-                sensorTextView.setText(sensorList);
                 break;
             case R.id.action_show_data:
-                sensorTextView.setText("Waiting for sensor data...");
-                readSensors = true;
                 break;
             default:
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /* Checks if external storage is available for read and write */
-    public boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
     }
 
 }
