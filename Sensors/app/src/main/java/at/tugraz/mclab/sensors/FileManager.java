da5bfa53d1d957ee9c725c4f5f56dea37b714a96
Christian <christian.toth@student.tugraz.at>
2018-05-13 17:11:08 +0200

    fix file reading + bigger font size

--- Sensors/app/src/main/java/at/tugraz/mclab/sensors/FileManager.java
+++ Sensors/app/src/main/java/at/tugraz/mclab/sensors/FileManager.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 public class FileManager {
 
     //read training files
-    public static TrainRecord[] readTrainFile(InputStream fileName) throws IOException{
+    public static TrainRecord[] readTrainFile(InputStream fileName) throws IOException {
         //File file = new File(fileName);
         Scanner scanner = new Scanner(fileName);
 
@@ -27,18 +27,17 @@ public class FileManager {
 
         assert LabelOrNot == 1 : "No classLabel";// ensure that C is present in this file
 
-
         //transform data from file into TrainRecord objects
         TrainRecord[] records = new TrainRecord[NumOfSamples];
         int index = 0;
-        while(scanner.hasNext()){
+        while (scanner.hasNext()) {
             scanner.nextInt();
             double[] attributes = new double[NumOfAttributes];
             int classLabel = -1;
 
             //Read a whole line for a TrainRecord
-            for(int i = 0; i < NumOfAttributes; i ++){
-                attributes[i] = scanner.nextDouble();
+            for (int i = 0; i < NumOfAttributes; i++) {
+                attributes[i] = Double.parseDouble(scanner.next());
             }
 
             //Read classLabel
@@ -46,14 +45,13 @@ public class FileManager {
             assert classLabel != -1 : "Reading class label is wrong!";
 
             records[index] = new TrainRecord(attributes, classLabel);
-            index ++;
+            index++;
         }
 
         return records;
     }
 
-
-    public static TestRecord[] readTestFile(String fileName) throws IOException{
+    public static TestRecord[] readTestFile(String fileName) throws IOException {
         File file = new File(fileName);
         Scanner scanner = new Scanner(file).useLocale(Locale.US);
 
@@ -67,12 +65,12 @@ public class FileManager {
 
         TestRecord[] records = new TestRecord[NumOfSamples];
         int index = 0;
-        while(scanner.hasNext()){
+        while (scanner.hasNext()) {
             double[] attributes = new double[NumOfAttributes];
             int classLabel = -1;
 
             //read a whole line for a TestRecord
-            for(int i = 0; i < NumOfAttributes; i ++){
+            for (int i = 0; i < NumOfAttributes; i++) {
                 attributes[i] = scanner.nextDouble();
             }
 
@@ -81,32 +79,33 @@ public class FileManager {
             assert classLabel != -1 : "Reading class label is wrong!";
 
             records[index] = new TestRecord(attributes, classLabel);
-            index ++;
+            index++;
         }
 
         return records;
     }
 
-    public static String outputFile(TestRecord[] testRecords, String trainFilePath) throws IOException{
+    public static String outputFile(TestRecord[] testRecords, String trainFilePath) throws
+            IOException {
         //construct the predication file name
         StringBuilder predictName = new StringBuilder();
-        for(int i = 15; i < trainFilePath.length(); i ++){
-            if(trainFilePath.charAt(i) != '_')
+        for (int i = 15; i < trainFilePath.length(); i++) {
+            if (trainFilePath.charAt(i) != '_')
                 predictName.append(trainFilePath.charAt(i));
             else
                 break;
         }
-        String predictPath = "classification\\"+predictName.toString()+"_prediction.txt";
+        String predictPath = "classification\\" + predictName.toString() + "_prediction.txt";
 
         //ouput the prediction labels
         File file = new File(predictPath);
-        if(!file.exists())
+        if (!file.exists())
             file.createNewFile();
 
         FileWriter fw = new FileWriter(file);
         BufferedWriter bw = new BufferedWriter(fw);
 
-        for(int i =0; i < testRecords.length; i ++){
+        for (int i = 0; i < testRecords.length; i++) {
             TestRecord tr = testRecords[i];
             bw.write(Integer.toString(tr.predictedLabel));
             bw.newLine();
