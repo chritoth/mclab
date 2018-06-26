b0a096b68865e7b6b1423cfa494b9b43bcf99de1
Christian <christian.toth@student.tugraz.at>
2018-06-26 13:35:42 +0200

    add additional floor plan (furniture) constraints to speed up convergence

--- localizationApp/app/src/main/java/at/tugraz/mclab/localization/FloorPlan.java
+++ localizationApp/app/src/main/java/at/tugraz/mclab/localization/FloorPlan.java
@@ -19,17 +19,32 @@ public class FloorPlan {
         walls.add(new Line(new Position(14.00, 00.00), new Position(14.00, 84.17))); // Wall E
         walls.add(new Line(new Position(05.33, 18.50), new Position(14.00, 18.50))); // Wall F
         walls.add(new Line(new Position(05.33, 18.50), new Position(05.33, 43.50))); // Wall G
-        walls.add(new Line(new Position(00.00, 43.50), new Position(14.00, 43.50))); // Wall H
-        walls.add(new Line(new Position(04.33, 18.50), new Position(04.33, 47.50))); // Wall I
+        walls.add(new Line(new Position(05.33, 43.50), new Position(14.00, 43.50))); // Wall H
+        walls.add(new Line(new Position(04.33, 18.50), new Position(04.33, 45.50))); // Wall I
         walls.add(new Line(new Position(00.00, 18.50), new Position(04.33, 18.50))); // Wall J
         walls.add(new Line(new Position(00.00, 16.50), new Position(04.33, 16.50))); // Wall K
         walls.add(new Line(new Position(04.33, 08.50), new Position(04.33, 16.50))); // Wall L
         walls.add(new Line(new Position(00.00, 09.67), new Position(04.33, 09.67))); // Wall M
+        walls.add(new Line(new Position(00.00, 45.50), new Position(04.33, 45.50))); // Wall N
+        walls.add(new Line(new Position(00.00, 47.50), new Position(14.00, 47.50))); // Wall O
+        walls.add(new Line(new Position(10.00, 43.50), new Position(10.00, 45.50))); // Wall O
+        walls.add(new Line(new Position(10.00, 45.50), new Position(14.00, 45.50))); // Wall Q
+
+        walls.add(new Line(new Position(00.00, 00.90), new Position(03.75, 00.90))); // Furniture Lab South
+        walls.add(new Line(new Position(00.00, 02.56), new Position(03.75, 02.56))); // Furniture Lab South
+        walls.add(new Line(new Position(00.00, 04.22), new Position(03.75, 04.22))); // Furniture Lab South
+        walls.add(new Line(new Position(00.00, 05.88), new Position(03.75, 05.88))); // Furniture Lab South
+        walls.add(new Line(new Position(00.00, 07.54), new Position(03.75, 07.54))); // Furniture Lab South
+        walls.add(new Line(new Position(03.75, 00.00), new Position(03.75, 00.90))); // Furniture Lab South
+        walls.add(new Line(new Position(03.75, 02.56), new Position(03.75, 04.22))); // Furniture Lab South
+        walls.add(new Line(new Position(03.75, 05.88), new Position(03.75, 07.54))); // Furniture Lab South
 
         // add rooms to the floor plan
         rooms = new ArrayList<Room>();
-        rooms.add(new Room(new Position(00.00, 00.00), new Position(04.00, 09.67))); // Lab south I
-        rooms.add(new Room(new Position(04.00, 00.00), new Position(05.33, 09.50))); // Lab south II
+        rooms.add(new Room(new Position(00.00, 00.90), new Position(03.75, 02.56))); // Lab south
+        rooms.add(new Room(new Position(00.00, 04.22), new Position(03.75, 05.88))); // Lab south
+        rooms.add(new Room(new Position(00.00, 07.54), new Position(03.75, 09.67))); // Lab south
+        rooms.add(new Room(new Position(03.75, 00.00), new Position(05.33, 09.50))); // Lab south
         rooms.add(new Room(new Position(04.33, 09.50), new Position(05.33, 16.50))); // Aisle
         rooms.add(new Room(new Position(00.00, 16.50), new Position(14.00, 18.50))); // Aisle
         rooms.add(new Room(new Position(04.33, 18.50), new Position(05.33, 43.50))); // Aisle
